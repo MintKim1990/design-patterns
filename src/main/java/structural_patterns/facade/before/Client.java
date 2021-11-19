43ffc6a41f995abba6f0a7ef453543761bd9f184
DESKTOP-VI6CUSJ\82107 <rlaalsxo1990@naver.com>
2021-11-19 20:43:20 +0900

    flyweight 패턴, 퍼사드 패턴 추가

---
+++ src/main/java/structural_patterns/facade/before/Client.java
@@ -0,0 +1,38 @@
+package structural_patterns.facade.before;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        String to = "rlaalsxo1990@naver.com";
+        String from = "rlaalsxo1990@gmail.com";
+        String host = "127.0.0.1";
+
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Test Mail from Java Program");
+            message.setText("message");
+
+            Transport.send(message);
+
+        } catch (Exception error) {
+
+        }
+
+    }
+}
