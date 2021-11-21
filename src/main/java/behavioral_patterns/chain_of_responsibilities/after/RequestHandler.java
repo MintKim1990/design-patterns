a706cd9b75e74e9f53879d70b44a29df68c61c0e
DESKTOP-VI6CUSJ\82107 <rlaalsxo1990@naver.com>
2021-11-21 18:43:19 +0900

    책임연쇄패턴 추가

---
+++ src/main/java/behavioral_patterns/chain_of_responsibilities/after/RequestHandler.java
@@ -0,0 +1,16 @@
+package behavioral_patterns.chain_of_responsibilities.after;
+
+public abstract class RequestHandler {
+
+    private RequestHandler nextHandler;
+
+    public RequestHandler(RequestHandler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+    public void handle(Request request) {
+        if (nextHandler != null) {
+            nextHandler.handle(request);
+        }
+    }
+}
